c81b6beeb3581ae83747015ed37eb8659f06651f
RudyTrt <rudy.trouillet@gmail.com>
2023-11-24 14:00:00 +0100

    PDO::FETCH_OBJ

--- config/db.php
+++ config/db.php
@@ -6,6 +6,7 @@ $db_name="bagnoles";
 try {
     $pdo = new PDO("mysql:host=$servername;dbname=$db_name", $username, $password);
     $pdo->setAttribute(PDO::MYSQL_ATTR_USE_BUFFERED_QUERY, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
 }
 catch(PDOException $e) {
     echo "Connection failed: " .$e->getmessage();
